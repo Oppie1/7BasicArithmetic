4b90a2b86c106b6408ffd8824b0ebc5e94c680e2
Oppie1 <atindall729@gmail.com>
2024-12-15 13:31:14 -0600

    Add files via upload
    7BasicArithmetic

---
+++ 7BasicArithmetic/BasicArithmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 24 % 7; // % modulus operator just gives remainder
+	int y = 28 / 4; // Normal division
+	int b = 16 + 4; // etc. etc...
+	int a = 3 * (4 + 8); // Order of operations PEDMAS. associative property
+
+	cout << x << endl;
+	cout << y << endl;
+	cout << b << endl;
+	cout << a << endl;
+
+		return 0;
+
+}
